7a271a49994dd4822e62cdac468a575e5b1f8ab6
karpechenko <ckka@mail.ru>
2019-04-09 14:33:38 +0300

    Познакомился с переменными и ветвлениями

--- les01.py
+++ les01.py
@@ -1,3 +1,21 @@
 print('программа номер 1')
-print('Hello world!')
-input()
+print('Привет программист!')
+name = input('Ваше имя:')
+print(name, ', добро пожаловать в мир Python!')
+answer = input('Давайте поработаем? (Y/N)')
+if answer == 'y':
+    print('Отлично хозяин!')
+    print('Я умею:')
+    print('[1] - выведу список файлов')
+    print('[2] - выведу информацию о системе')
+    do = input('Укажите номер действия:')
+    if do == 1:
+        pass
+    elif do == 2:
+        pass
+    else:
+        pass
+elif answer == 'n':
+    print('До свидания!')
+else:
+    print('Неизвестный ответ')
