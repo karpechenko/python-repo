c29a52019d1522e1b3e6e855ddb0ef3ca9e527bd
karpechenko <ckka@mail.ru>
2019-04-10 12:24:30 +0300

    Познакомился с функциями: type, dir, help. Познакомился с подключением модулей Доработал программу помощника.

--- les01.py
+++ les01.py
@@ -1,20 +1,35 @@
+import os
+import psutil
+import sys
+
+
 print('программа номер 1')
 print('Привет программист!')
 name = input('Ваше имя:')
 print(name, ', добро пожаловать в мир Python!')
 answer = input('Давайте поработаем? (Y/N)')
+
 if answer == 'y':
     print('Отлично хозяин!')
     print('Я умею:')
     print('[1] - выведу список файлов')
     print('[2] - выведу информацию о системе')
-    do = input('Укажите номер действия:')
+    print('[3] - Выведу список процессов')
+    do = int(input('Укажите номер действия:'))
+    
     if do == 1:
-        pass
+        print(os.listdir())
     elif do == 2:
-        pass
+        print('Логин пользователя: ' + os.getlogin())
+        print('Количество CPU: ' + str(os.cpu_count()))
+        print('Кодировка файловой системы: ' + sys.getfilesystemencoding())
+        print('Текущая директория: ' + os.getcwd())
+        print('Платформа (ОС): ' + sys.platform)
+    elif do == 3:
+        print(psutil.pids())
     else:
         pass
+    
 elif answer == 'n':
     print('До свидания!')
 else:
